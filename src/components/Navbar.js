6da4b0bdf682247b3ead9e036e1508ac6f2e40cc
Paras Sachdeva <parassachdeva0111@gmail.com>
2024-04-15 21:16:53 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <Link to={"/"} className="navbar-brand text-white">
-            <h4>React Movie App</h4>
+            <h4>STREAMIFY</h4>
           </Link>
           <div className="d-flex text-white align-items-center">
             {currentUser ? (
